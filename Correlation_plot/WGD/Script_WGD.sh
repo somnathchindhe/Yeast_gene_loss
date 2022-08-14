54e4129db04d9b2a55681499489a3e631e9e1bf8
somnathchindhe <104543925+somnathchindhe@users.noreply.github.com>
2022-08-14 18:03:34 +0530

    Update Script_WGD.sh

--- Correlation_plot/WGD/Script_WGD.sh
+++ Correlation_plot/WGD/Script_WGD.sh
@@ -2,8 +2,8 @@
 #script need input as Gene satble id lists of two pathway and give output as correlation plot of gene between two pathway in yeast WGD species 
 #To Run script requires Piilers.tab, combinatiion list, file1.txt ,file2.txt and WGD.R files
 #Input Gene stable  id list file should have file name same as pathway name 
-a="Vitamin_metabolism"                  #Input =pathway 1 gene stable id list eg.a="lipid_metabolism'
-b="RNA_metabolism"                  #Input= pathway 2 gene stable id list  eg. b='Glucose_metabolism"
+a=""                  #Input =pathway 1 gene stable id list eg.a="lipid_metabolism'
+b=""                  #Input= pathway 2 gene stable id list  eg. b='Glucose_metabolism"
 
 #Get pathway genes from pillerstab
 for i in `cat "$a"`
